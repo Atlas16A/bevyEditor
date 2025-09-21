ee93585dad1a09f9d1d414109be1040d014e14a2
Atlas16A <thegoldenatlaspass@gmail.com>
2025-09-21 17:17:15 -0400

    add cli

--- bins/bevy_launcher/src/main.rs
+++ bins/bevy_launcher/src/main.rs
@@ -1,5 +1,12 @@
 //! Internal Bevy Launcher plugin, where the bevy launcher is built.
 use bevy::prelude::*;
+use bevy_cli::commands::{
+    build::{build, BuildArgs, BuildSubcommands, BuildWebArgs},
+    completions::completions,
+    lint::{lint, InstallArgs, LintArgs, LintSubcommands},
+    new::{new, NewArgs},
+    run::{run, RunArgs, RunSubcommands, RunWebArgs},
+};
 
 /// The internal Bevy Launcher plugin.
 pub struct LauncherInternalPlugin;
